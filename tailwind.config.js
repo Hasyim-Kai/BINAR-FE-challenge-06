2843ed55194a5d644d12eb765cc6aca4512bfa21
Kai-69 <ichihatsu1@gmail.com>
2022-04-27 08:54:18 +0700

    Install Tailwind and other dependencies

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        darkblue: '#0D28A6',
+        backgroundGrey: '#F1F3FF',
+        primaryGrey: '#CFD4ED'
+      }
+    },
+  },
+  plugins: [],
+}
