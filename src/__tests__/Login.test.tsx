de85baf82ea796946e35d91e5b4c8f22c7809f71
ichihatsu1 <ichihatsu1@gmail.com>
2022-04-29 08:05:40 +0700

    Feat : Add Login Page Test

---
+++ src/__tests__/Login.test.tsx
@@ -0,0 +1,34 @@
+import { render, screen } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
+import LoginPage from "../pages/LoginPage";
+
+
+describe('Login Page Test', () => {
+    it('Should have email input field', () => {
+        render(<BrowserRouter><LoginPage /></BrowserRouter>);
+        const elemet = screen.getByPlaceholderText(/Contoh: johndee@gmail.com/i)
+        expect(elemet).toBeInTheDocument();
+        expect(elemet).toBeVisible();
+    })
+
+    it('Should have Password input field', () => {
+        render(<BrowserRouter><LoginPage /></BrowserRouter>);
+        const elemet = screen.getByPlaceholderText('6+ karakter')
+        expect(elemet).toBeInTheDocument();
+        expect(elemet).toBeVisible();
+    })
+
+    it('Should have Login Button', () => {
+        render(<BrowserRouter><LoginPage /></BrowserRouter>);
+        const elemet = screen.getByRole('button', { name: 'Sign In', })
+        expect(elemet).toBeInTheDocument();
+        expect(elemet).toBeVisible();
+    })
+
+    it('Should have Login with Google Button', () => {
+        render(<BrowserRouter><LoginPage /></BrowserRouter>);
+        const elemet = screen.getByText(/login with google/i)
+        expect(elemet).toBeInTheDocument();
+        expect(elemet).toBeVisible();
+    })
+});
