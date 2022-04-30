df6a148349db0fab1f37d6aa8b62dfc1b0374e56
Kai-69 <ichihatsu1@gmail.com>
2022-05-01 06:53:13 +0700

    Add blank Home Page

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,12 @@
+export default function HomePage() {
+    const flexDisplay = `flex flex-col lg:flex-row gap-5  bg-backgroundGrey`;
+
+    return <div className={flexDisplay}>
+        {/* <div className="ml-9 lg:ml-14 xl:ml-24 2xl:ml-48 my-16 lg:my-auto flex-1">
+            <h2 className="text-4xl font-bold mb-4">Sewa & Rental Mobil Terbaik di kawasan (Lokasimu)</h2>
+            <p className="max-w-md">Selamat datang di Binar Car Rental. Kami menyediakan mobil kualitas
+                terbaik dengan harga terjangkau. Selalu siap melayani kebutuhanmuuntuk sewa mobil selama 24 jam.</p>
+        </div>
+        <img className="flex-1" src="./images/search_page_car.png" alt="Car" /> */}
+    </div>
+}
