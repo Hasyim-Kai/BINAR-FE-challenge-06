e914968bbd15b583fad1ee1e830394f6a99d373b
Kai-69 <ichihatsu1@gmail.com>
2022-05-07 08:10:04 +0700

    Refactor : Update Routes

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,11 +1,15 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import DashboardPage from "./pages/DashboardPage";
+import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
+import NotFound from "./pages/NotFound";
+import RegisterPage from "./pages/RegisterPage";
 
 export default function AppRoutes() {
    return <BrowserRouter >
       <Routes>
-         <Route index element={<LoginPage />} />
-         <Route path="register" element={<RegisterPage />} />
+         <Route index element={<RegisterPage />} />
+         <Route path="login" element={<LoginPage />} />
          <Route path="home" element={<HomePage />} />
          <Route path="admin" element={<DashboardPage />} />
          <Route path='*' element={<NotFound />} />
